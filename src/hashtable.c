46cae15e865def45784c55102c2d626263af34e2
yoav <yoav.shalev67@gmail.com>
2020-04-10 12:43:50 +0300

    Update implement functions and adding includes

--- src/hashtable.c
+++ src/hashtable.c
@@ -5,22 +5,29 @@ file: hashtable.
 */
 #include "../include/hashtable.h"
 
-static int curr_size;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void init_data()
+pnode init_data() /*value range of size: 0 to 255*/
 {
 	int i;
-	for(i = 0; i < HASH_SIZE; i++)
-	{
-		hash_table[i]->p = (pair*)malloc(sizeof(pair));
-		if(hash_table[i]->p == NULL)
-			printf("Error: in index %d ", i);
+	pnode table = NULL;
 
-		hash_table[i]->p->key = NULL;
-		hash_table[i]->p->value = 0;
+	/*Allocate the table itself*/
+	if((table = (pnode)malloc(sizeof(node))) == NULL)
+		return NULL;
+
+	for (i = 0; i < HASH_SIZE; i++)
+	{
+		if((table[i] = (node*)malloc(sizeof(node))) == NULL)
+			return NULL;
+
+		table[i]->pair = (pair*)calloc(2, sizeof(pair));
+		table[i]->next = NULL;
 	}
 
-	curr_size = 0;
+	return table;
 }
 
 int hashCode(char* key)
@@ -29,17 +36,20 @@ int hashCode(char* key)
 	hash = index = i = 0;
 
 	while(key[i])
+	{
 		hash+=key[i];
+		i++;
+	}
 	index = hash % HASH_SIZE;
 
 	return index;
 }
 
 /*Count no. of node in list*/
-int getCount(char* key)
+int getCount(pnode table, char* key)
 {
 	int count = 0;
-	node* cur = hash_table[hashCode(key)];
+	node* cur = table[hashCode(key)];
 
 	while(cur != NULL)
 	{
@@ -49,52 +59,56 @@ int getCount(char* key)
 	return count;
 }
 
-int contains(char* key)
+int contains(pnode table, char* key)
 {
+
 	int i, size;
-	node* vec = hash_table[hashCode(key)];
-	size = getCount(key);
+	node* vec = table[hashCode(key)];
+	size = getCount(table, key);
 
 	for(i = 0; i < size; i++)
 	{
-		if(strcmp(vec->p->key, key))
-			return TRUE;
+		if(vec->pair->key[0] != '\0')
+			if(strcmp(vec->pair->key, key) == 0)
+				return TRUE;
+
 		vec = vec-> next;
 	}
-	printf("Error: %s not found!\n", key);
+	printf("%s not found!\n", key);
 	return FALSE;
 }
 
-void insert(char* key, int value)
+void insert(pnode table, char* key, int value)
 {
+	int i;
 	node* vec = NULL;
-	if(contains(key))
+	if(contains(table, key))
 	{
 		printf("Error: key %s is already exists!\n", key);
 		return;
 	}
-	
-	vec = hash_table[hashCode(key)];
-	strcpy(vec->p->key, key);
-	vec->p->value = value;
+	i = hashCode(key);
+	vec = table[i];
+	vec->pair->key = key;
+	vec->pair->value = value;
 
 	/*make next of vec as head */
-	if(getCount(key) > 0)
-		vec->next = hash_table[hashCode(key)];
+	if(getCount(table, key) > 1)
+		vec->next = table[i];
 
 	/*move the head to point to the new node*/
-	hash_table[hashCode(key)] = vec;
+	table[i] = vec;
 }
 
-node* get(char* key)
+node* get(pnode table, char* key)
 {
 	int i, size;
-	node* vec = hash_table[hashCode(key)];
-	size = getCount(key);
+	node* vec = table[hashCode(key)];
+	size = getCount(table, key);
 
 	for(i = 0; i < size; i++)
 	{
-		if(strcmp(vec->p->key, key))
+		if(strcmp(vec->pair->key, key))
 			return vec;
 		vec = vec-> next;
 	}
@@ -103,28 +117,22 @@ node* get(char* key)
 }
 
 /*python printing style*/
-void print()
+void print(pnode table)
 {
 	int i, j, size;
 	node* vec = NULL;
-
-	if(curr_size == 0)
-	{
-		printf("{}\n");
-		return;
-	}
 
 	printf("{");
 
 	for(i = 0; i < HASH_SIZE; i++)
 	{
-		vec = hash_table[i];
-		size = getCount(vec->p->key);	
+		vec = table[i];
+		size = getCount(table, vec->pair->key);	
 		for(j = 0; j < size; j++)
 		{
-			printf("%s",vec->p->key);
+			printf("%s",vec->pair->key);
 			printf(":");
-			printf("%d",vec->p->value);
+			printf("%d",vec->pair->value);
 			if(i < HASH_SIZE)
 				printf(", ");
 		}
