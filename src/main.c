1d0b857a03bc6dedd99f595292ddf9892c8cbe06
yoav <yoav.shalev67@gmail.com>
2020-05-01 16:29:52 +0300

    adding declers function on hash table

--- src/main.c
+++ src/main.c
@@ -9,8 +9,14 @@ file: main.c
 int main()
 {
 	node** table = init_table();
+	pair* temp = NULL;
+
+	erase(table, "MAIN");
 
 	insert(table, "MAIN", 100);
+	temp = get(table, "MAIN");
+
+	printf("%s: %d\n",temp->key, temp->value);
 
 	insert(table, "LOOP", 103);
 
@@ -19,6 +25,11 @@ int main()
 	insert(table, "STR", 125);
 	insert(table, "LIST", 130);
 	insert(table, "K", 133);
+
+	print(table);
+
+	erase(table, "MAIN");
+
 	print(table);
 	
 	clear_table(table);
