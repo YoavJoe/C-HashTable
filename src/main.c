7ab60b085b692c31c2fc40ea81f896bfe8bb3fab
yoav <yoav.shalev67@gmail.com>
2020-05-01 12:25:39 +0300

    adding pairs to insert into thehash table

--- src/main.c
+++ src/main.c
@@ -9,11 +9,19 @@ file: main.c
 int main()
 {
 	node** table = init_table();
-	insert(table, "K", 8);
+
+	insert(table, "MAIN", 100);
+
+	insert(table, "LOOP", 103);
+
+	insert(table, "END", 124);
+
+	insert(table, "STR", 125);
+	insert(table, "LIST", 130);
+	insert(table, "K", 133);
 	print(table);
 	
 	clear_table(table);
 
 	return 0;
 }
-
